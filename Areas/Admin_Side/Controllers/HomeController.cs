84af2072d5a6a0b9b44518de26a66e750e6af377
swapnilkotadiya999@gmail.com <swapnilkotadiya999@gmail.com>
2023-04-16 01:25:18 +0530

    1.25

---
+++ Areas/Admin_Side/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using The_Ghar.Areas.Admin_Side.Models;
+using The_Ghar.BALAdmin;
+using The_Ghar.DAL;
+
+namespace The_Ghar.Areas.Admin_Side.Controllers
+{
+    [CheckAccess]
+    [Area("Admin_Side")]
+    [Route("Admin_Side/[controller]/[action]")]
+
+    public class HomeController : Controller
+    {
+        Admin_Side_DAL dalHome = new Admin_Side_DAL();
+        private IConfiguration Configuration;
+        public HomeController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        #region Home List
+        public IActionResult HomeList()
+        {
+            DataTable dt = dalHome.dbo_PR_Home_SelectAll();
+            return View("HomeList", dt);
+        }
+        #endregion
+
+        #region Home Delete
+        public IActionResult HomeDelete(int HomeID)
+        {
+
+
+            DataTable dt = dalHome.dbo_PR_Home_DeleteByPK(HomeID);
+
+            return RedirectToAction("HomeList");
+
+        }
+        #endregion
+
+        #region "Home Add"
+        public IActionResult HomeAdd(int HomeID)
+        {
+            if (HomeID != null)
+            {
+
+                DataTable dt = dalHome.dbo_PR_Admin_GetValue_For_Edit(HomeID);
+                HomeModel modelHome = new HomeModel();
+                if (dt.Rows.Count > 0)
+                {
+
+                    foreach (DataRow dr in dt.Rows)
+                    {
+
+                        modelAdmin.AdminID = Convert.ToInt32(dr["AdminID"]);
+                        modelAdmin.Name = (string)dr["Name"];
+                        modelAdmin.Email = (string)dr["Email"];
+                        modelAdmin.Password = (string)dr["Password"];
+                        modelAdmin.CreationDate = Convert.ToDateTime(dr["CreationDate"]);
+                        modelAdmin.ModificationDate = Convert.ToDateTime(dr["ModificationDate"]);
+                    }
+
+                    return View("HomeAddEdit", modelHome);
+                }
+            }
+            return View("AdminAddEdit");
+        }
+
+        #endregion
+
+        #region "AdminSave"
+
+        public IActionResult AdminSave(AdminModel modelAdmin)
+        {
+
+
+
+            if (modelAdmin.AdminID == 0)
+            {
+                DataTable dt = dalAdmin.dbo_PR_Admin_Insert(modelAdmin);
+                TempData["AdminInsertMsg"] = "Record Inserted Successfully";
+            }
+            else
+            {
+                DataTable dt = dalAdmin.dbo_PR_Admin_UpdateByPK(modelAdmin);
+                TempData["AdminUpdateMsg"] = "Record Updated Successfully";
+            }
+
+
+
+            return View("AdminAddEdit");
+
+        }
+        #endregion
+
+    }
+}
---
+++ Areas/Admin_Side/Models/HomeModel.cs
@@ -0,0 +1,41 @@
+﻿namespace The_Ghar.Areas.Admin_Side.Models
+{
+    public class HomeModel
+    {
+        public int HomeID { get; set; }
+
+        public string HomeName { get; set; }
+
+        public string Email { get; set; }
+
+        public string AreaLocation { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Logo { get; set; }
+
+        public int StateID { get; set; }
+
+        public int CityID { get; set; }
+
+        public string CostPerPerson { get; set; }
+
+        public string Categories { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ModificationDate { get; set; }
+
+        public int HomeOwnerID { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string OwnerEmail { get; set; }
+
+        public string OwnerMobile { get; set; }
+
+        public string Password { get; set; }
+
+
+    }
+}
